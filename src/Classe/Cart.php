8cb61f173b10b9aad92bcc74b892a7389acfdec7
wa2l-99 <waelzantour1@gmail.com>
2021-01-05 00:26:59 +0100

    Chore: Créer un panier et une classe "Cart()"

---
+++ src/Classe/Cart.php
@@ -0,0 +1,57 @@
+<?php
+//contient tous la mécanique du panier (Ajout, Suppression, Affichage, )
+
+namespace App\Classe;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+
+
+class Cart {
+
+    private $session;
+
+    public function __construct(EntityManagerInterface $entityManager,SessionInterface $session)
+    
+    {
+        $this->session = $session;
+        $this->entityManager = $entityManager;
+    
+
+    }
+    
+    public function add($id)
+    {
+        $cart = $this->session->get('cart', []);
+
+        if (!empty($cart[$id])) {
+            $cart[$id]++;
+        } else {
+            $cart[$id] = 1;
+        }
+
+        $this->session->set('cart', $cart);
+
+    }
+
+
+    public function get(){
+
+        return $this->session->get('cart');
+
+
+    }
+
+    public function remove(){
+
+        return $this->session->remove('cart');
+
+
+    }
+
+
+  
+
+
+}
---
+++ src/Controller/CartController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use App\Classe\Cart;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CartController extends AbstractController
+{
+    /**
+     * @Route("/mon-panier", name="cart")
+     */
+    public function index(Cart $cart)
+    {
+        
+        
+        return $this->render('cart/index.html.twig',[
+
+            'cart'=>$cart->get()
+
+        ]);
+    }
+
+    /**
+     * @Route("/cart/add/(id)", name="add-to-cart")
+     */
+    public function add(Cart $cart,$id)
+    {
+
+        $cart-> add($id);
+
+        return $this->redirectToRoute('cart');
+
+
+    }
+
+     /**
+     * @Route("/cart/remove", name="remove-my-cart")
+     */
+    public function remove(Cart $cart)
+    {
+
+        $cart-> remove();
+
+        return $this->redirectToRoute('products');
+
+
+    }
+}
