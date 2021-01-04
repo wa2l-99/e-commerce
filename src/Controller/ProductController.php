4b09a92aac4d6e9f8866f9e64ca1d5f4cc5739dd
Sahar.Achour <sahar.achour1999@gmail.com>
2021-01-04 05:42:25 +0100

    chore:cree un vue de tous les produits

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,50 @@
 
 namespace App\Controller;
 
+use App\Classe\Search;
+use App\Entity\Product;
+use App\Form\SearchType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class ProductController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/nos-produits", name="products")
      */
-    public function index(): Response
+    public function index()
     {
-        return $this->render('product/index.html.twig');
+    
+            $products = $this->entityManager->getRepository(Product::class)->findAll();
+
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products
+        ]);
+    }
+
+    /**
+     * @Route("/produit/{slug}", name="product")
+     */
+    public function show($slug)
+    {
+        $product = $this->entityManager->getRepository(Product::class)->findOneBySlug($slug);
+        if (!$product) {
+            return $this->redirectToRoute('products');
+        }
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+        ]);
     }
 }
