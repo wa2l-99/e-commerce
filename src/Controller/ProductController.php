acc295d3a428e4161449d503ebab810101b3478b
Sahar.Achour <sahar.achour1999@gmail.com>
2021-01-04 07:58:15 +0100

    chore:création d'un bar de filtre de produits

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -10,7 +10,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-
 class ProductController extends AbstractController
 {
     private $entityManager;
@@ -23,14 +22,22 @@ class ProductController extends AbstractController
     /**
      * @Route("/nos-produits", name="products")
      */
-    public function index()
+    public function index(Request $request)
     {
-    
-            $products = $this->entityManager->getRepository(Product::class)->findAll();
+        $search = new Search();
+        $form = $this->createForm(SearchType::class, $search);
 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $products = $this->entityManager->getRepository(Product::class)->findWithSearch($search);
+        } else {
+            $products = $this->entityManager->getRepository(Product::class)->findAll();
+        }
 
         return $this->render('product/index.html.twig', [
-            'products' => $products
+            'products' => $products,
+            'form' => $form->createView()
         ]);
     }
 
@@ -40,12 +47,15 @@ class ProductController extends AbstractController
     public function show($slug)
     {
         $product = $this->entityManager->getRepository(Product::class)->findOneBySlug($slug);
+        $products = $this->entityManager->getRepository(Product::class)->findByIsBest(1);
+
         if (!$product) {
             return $this->redirectToRoute('products');
         }
 
         return $this->render('product/show.html.twig', [
             'product' => $product,
+            'products' => $products
         ]);
     }
 }
