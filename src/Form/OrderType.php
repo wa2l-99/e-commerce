ade3a4d29be487b55586148181681752c93899b9
wa2l-99 <waelzantour1@gmail.com>
2021-01-10 21:34:34 +0100

    Chore:Ajouter form pour choisir l'add de livraison

---
+++ src/Form/OrderType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Address;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OrderType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $user = $options['user'];
+
+
+        $builder
+            ->add('addresses',EntityType::class,[
+                'label' => 'Choisisser votre adresse de livraison',
+                'required' => true,
+                'class' => Address::class,
+                'choices' => $user->getAddresses(),
+                'multiple' => false,
+                'expanded' => true
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // recuperer le variable user passer dans le controlleur 
+            'user' => array()
+        ]);
+    }
+}
